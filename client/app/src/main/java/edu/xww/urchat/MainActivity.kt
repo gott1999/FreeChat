fdf111b764c4dcf4c46ef9d1172a1c8a5e420dea
xiangweiwei <1261427646@qq.com>
2022-03-04 14:17:51 +0800

    Add client

---
+++ client/app/src/main/java/edu/xww/urchat/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.xww.urchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
